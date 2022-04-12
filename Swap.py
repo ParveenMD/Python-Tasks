ed1a12bf0a7367514132e40470dcc39e20e129c7
MD Parveen <100269535+ParveenMD@users.noreply.github.com>
2022-04-12 14:56:33 +0000

    Update Swap.py

--- Swap.py
+++ Swap.py
@@ -1,9 +1,7 @@
-var1= int(input("Enter value of var1:"))
-var2= int(input("Enter value of var2:"))
-print("\nBEFORE SWAPPING(Actual Value of var):Value of var1:{}".format(var1))
-print("BEFORE SWAPPING(Actual value of var):Value of var2:{}".format(var2))
-temp_var=var1
-var1=var2
-var2=temp_var
-print("\nAFTER SWAPPING:Value of var1:{}".format(var1))
-print("AFTER SWAPPING:Value of var2:{}".format(var2))
+num1 = int(input("enter the first number:"))
+num2 = int(input("enter the second number:"))
+temp = num1
+num1 = num2
+num2 = temp
+print("value of num1:", num1)
+print("value of num2:", num2)
