bd00273a1dbf37dfd0915b8ae3331a5c36683bce
MD Parveen <100269535+ParveenMD@users.noreply.github.com>
2022-04-12 14:58:50 +0000

    Delete Swap.py

--- Swap.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("enter the first number:"))
-num2 = int(input("enter the second number:"))
-temp = num1
-num1 = num2
-num2 = temp
-print("value of num1:", num1)
-print("value of num2:", num2)
